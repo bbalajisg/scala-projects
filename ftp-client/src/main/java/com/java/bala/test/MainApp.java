836489c678ae4c53f1afc3c0e506b96252a04a75
bbalajisg <bbalaji.sg@gmail.com>
2014-03-26 14:03:38 +0800

    test

--- ftp-client/src/main/java/com/java/bala/test/MainApp.java
+++ ftp-client/src/main/java/com/java/bala/test/MainApp.java
@@ -3,7 +3,7 @@ package com.java.bala.test;
 public class MainApp {
 	
 	public static void main(String[] args){
-		System.out.println("Java main application....  :) ");
+		System.out.println("Java main app....  :) ");
 	}
 
 }
