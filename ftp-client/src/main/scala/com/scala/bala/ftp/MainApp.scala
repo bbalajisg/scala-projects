5dbd223cfb84d57fbbb0a434d381d504b5d2cd52
bbalajisg <bbalaji.sg@gmail.com>
2014-04-16 09:36:04 +0800

    Fixed Bug

--- ftp-client/src/main/scala/com/scala/bala/ftp/MainApp.scala
+++ ftp-client/src/main/scala/com/scala/bala/ftp/MainApp.scala
@@ -9,38 +9,27 @@ import akka.routing.RoundRobinRouter
 
 object MainApp {
 
- 
-  
   def main(args: Array[String]): Unit = {
    
-   /**
-    * Call to read configuration file.
-    */ 
+     /**
+      * Call to read configuration file.
+      */
        
     println("FTP Client started...");
-        
-    val serverIps = FTPConfigurationReader.severIPs        
+
+    val serverIps = FTPConfigurationReader.severIPs
     val userName = FTPConfigurationReader.userNames
     val password = FTPConfigurationReader.passwords
-        
-    val zippedCredentials = (serverIps zip userName) zip password     
+
+    val zippedCredentials = (serverIps zip userName) zip password
     val credentials =  zippedCredentials map {
-      
-    	case ((a,b),c) => Array(a.trim(),b.trim(),c.trim())
-    	
+        case ((a,b),c) => Array(a.trim(),b.trim(),c.trim())
     }
-    
-     
-  /*  val serverDetails = new Servers()        
-    serverDetails.start
-  
-    credentials.foreach( serverDetails ! ServerConnection(_) )*/
-    
-    
+
     val system = ActorSystem()
     val server = system.actorOf(Props(new Servers).withRouter(RoundRobinRouter(nrOfInstances = 10)), "Servers")
-    
-    credentials.foreach( server ! ServerConnection.connect(_) )
+
+    credentials.foreach(server ! ServerConnection.connect(_) )
           
   } 
 
--- ftp-client/src/main/scala/com/scala/bala/ftp/Servers.scala
+++ ftp-client/src/main/scala/com/scala/bala/ftp/Servers.scala
@@ -1,28 +1,22 @@
 package com.scala.bala.ftp
 
 import akka.actor.Actor
-import com.scala.bala.ftp.util.FTPConfigurationReader
 import akka.actor.Props
 import akka.routing.RoundRobinRouter
  
 	object ServerConnection {	    
-		case class connect(c:Array[String])	    
-		 
+		  case class connect(c:Array[String])
 	}
 	
 	class Servers() extends Actor{
-		
 		 val download = context.actorOf(Props(new Downloaders).withRouter(RoundRobinRouter(nrOfInstances = 10)), "downloaders")
 	  
 		def receive = {
-			case ServerConnection.connect(c) =>  createServerFolders(c)	
-			 
+			case ServerConnection.connect(c) =>  createServerFolders(c)
 		}
 		 
 		private def createServerFolders(credential:Array[String]) = {
-			
-			 download !  Download.downloadFiles(credential) 
-			 
+			 download !  Download.downloadFiles(credential)
 		} 
    
 	}
