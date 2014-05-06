6cb26e9435a45254bfe15e29fa693e4d8447c062
bbalajisg <bbalaji.sg@gmail.com>
2014-05-06 17:07:57 +0800

    Initial version

--- spring-mongodb-interact/src/main/java/com/bala/interact/DBMainApp.java
+++ spring-mongodb-interact/src/main/java/com/bala/interact/DBMainApp.java
@@ -1,7 +1,9 @@
 package com.bala.interact;
 
-import com.bala.interact.com.bala.interact.config.SpringMongoDBConfig;
-import com.bala.interact.modal.User;
+import com.bala.interact.conf.SpringMongoDBConfig;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.data.mongodb.core.MongoOperations;
@@ -17,13 +19,23 @@ public class DBMainApp {
         System.out.print("Hey I am in  job");
 
         ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringMongoDBConfig.class);
-        MongoOperations mongoOperation = (MongoOperations) ctx.getBean("mongoTemplate");
+        MongoOperations mongoOpsContext = (MongoOperations) ctx.getBean("mongoTemplate");
 
-        User user = new User("balab", "password123");
+        DBCollection albums = mongoOpsContext.getCollection("albums");
+        DBCursor albumCursor = albums.find();
 
+        System.out.print("Count albums : " + albums.count());
 
+        DBCollection images = mongoOpsContext.getCollection("images");
+        DBCursor imagesCursor = images.find();
 
-        mongoOperation.save(user);
+        System.out.print("Count images : " + images.count());
+
+        while (imagesCursor.hasNext()) {
+            DBObject records = imagesCursor.next();
+            //System.out.println(records);
+        }
+
 
         System.out.print("Hey I am done with job");
     }
